382254efcd4f972157bafa680852a0c98bb41f42
hyamaguchi <hyamaguchi@xcoo.jp>
2025-02-24 22:05:18 +0900

    a37

---
+++ problem/tessoku/a37.py
@@ -0,0 +1,66 @@
+import math
+
+INF = 1 << 60
+MOD1 = 10**9 + 7
+
+
+def int_input():
+    return int(input())
+
+
+def int_list(dec=False, to_tuple=False):
+    result = [int(x) - dec for x in input().split()]
+    return tuple(result) if to_tuple else result
+
+
+def int_row(n, dec=False):
+    return [int_input() - dec for _ in range(n)]
+
+
+def int_row_list(n, dec=False, to_set=False):
+    return (
+        {int_list(dec, to_tuple=True) for _ in range(n)}
+        if to_set
+        else [int_list(dec) for _ in range(n)]
+    )
+
+
+def str_input():
+    return input()
+
+
+def str_list(to_tuple=False):
+    result = list(input().split())
+    return tuple(result) if to_tuple else result
+
+
+def str_row(n):
+    return [str_input() for _ in range(n)]
+
+
+def str_row_list(n):
+    return [str_list() for _ in range(n)]
+
+
+def grid_input(h):
+    return [list(input().strip()) for _ in range(h)]
+
+
+def string_join(sep, arr):
+    return sep.join(map(str, arr))
+
+
+def main():
+    n, m, b = int_list()
+    A = int_list()
+    C = int_list()
+
+    train_to_x = sum([m * a for a in A])
+    y_to_bus = sum([n * c for c in C])
+    x_to_y = b * n * m
+
+    return train_to_x + y_to_bus + x_to_y
+
+
+if __name__ == "__main__":
+    print(main())
